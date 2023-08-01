ecfdd64deb132f36597872a865dd564cc23ef9d1
sandwichesIN <129165814+sandwichesIN@users.noreply.github.com>
2023-08-01 19:45:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('button_dr');
+    const audio = document.getElementById('crunch-sound');
+
+    // Check if the button and audio elements are found before adding the event listener
+    if (button && audio) {
+        button.addEventListener('click', function () {
+            audio.play();
+            setTimeout(function () {
+                window.location.href = 'door-step_del.html';
+            }, 1000); // Add a delay of 1 second before navigating to the next page
+        });
+    }
+});
