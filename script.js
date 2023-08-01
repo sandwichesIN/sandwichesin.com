2dc52ab17902b821cf43134c0921a9f433b5e31b
sandwichesIN <129165814+sandwichesIN@users.noreply.github.com>
2023-08-01 20:13:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('button_pk');
+    const audio = document.getElementById('crunch-sound');
+
+    // Check if the button and audio elements are found before adding the event listener
+    if (button && audio) {
+        button.addEventListener('click', function () {
+            audio.play();
+            setTimeout(function () {
+                window.location.href = 'cl_kitchen_pk.html';
+            }, 1000); // Add a delay of 1 second before navigating to the next page
+        });
+    }
+});
